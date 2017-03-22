f8fb668d7f5913464ebb2ca1818ab5330d75581c
Dhash Shrivathsa <dhash@outlook.com>
2017-03-22 04:15:15 -0400

    Added spec and route handlers

--- src/main/scala-2.12/mortar/Server.scala
+++ src/main/scala-2.12/mortar/Server.scala
@@ -9,22 +9,26 @@ import squants.information.Information
 
 import scala.io.StdIn
 import scala.concurrent.Future
-
 import java.io.File
+
+import akka.http.javadsl.server.PathMatchers
+
 import scala.io.Source
-
 import squants.information.Bytes
-import akka.http.scaladsl.server.Directives
+import squants.information.Information
 import akka.http.scaladsl.server.Route
-
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.server.Directives._
+import akka.pattern.ask
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json._
-
 import mortar.spec._
-
 import org.pmw.tinylog.Logger
 
-class Server(config: ApplicationConfig) extends Directives with MortarJsonSupport {
+class Server(config: ApplicationConfig)
+    extends Directives
+    with MortarJsonSupport {
   // needed to run the route
   private implicit val system = ActorSystem("mortar-http-server-actorsystem")
   private implicit val materializer = ActorMaterializer()
@@ -36,19 +40,17 @@ class Server(config: ApplicationConfig) extends Directives with MortarJsonSuppor
       //How much free space does the server have
       path("free_space") {
         get {
-          val freeSpace: Future[Information] = Future {
-            Bytes(new File(".").getFreeSpace)
-          }
-          onSuccess(freeSpace) {
-            space => complete(FreeSpace(space))
+          val freeSpace: Future[Information] = Future { getFreeSpace }
+          onSuccess(freeSpace) { space =>
+            complete(FreeSpace(space))
           }
         }
-      //what is the server's pubkey
+        //what is the server's pubkey
       } ~ path("pubkey") {
         get {
           val key: Future[Option[String]] = Future {
-            try{
-              val pkeyfile = new File(config.local.sec.ssh.pub)
+            try {
+              val pkeyfile = new File(config.local.sec.ssh.pub) //grab my pubkey
               Some(Source.fromFile(pkeyfile).mkString)
             } catch {
               case e: Throwable =>
@@ -57,8 +59,8 @@ class Server(config: ApplicationConfig) extends Directives with MortarJsonSuppor
                 None
             }
           }
-          onSuccess(key) {
-            k => k match {
+          onSuccess(key) { k =>
+            k match {
               case Some(pkey) => complete(PubKey(pkey))
               case None => complete("No Pubkey")
             }
@@ -66,20 +68,60 @@ class Server(config: ApplicationConfig) extends Directives with MortarJsonSuppor
         }
       } ~ path("allocate") {
         post {
-          val fs = Http().singleRequest(HttpRequest(uri = s"localhost:${config.app.port}/free_space"))
-          onSuccess(fs) {
-            space => entity(as[FreeSpace]) {
-              reqSpace: FreeSpace => reqSpace.-()
+          entity(as[SpaceRequest]) { req =>
+            val self_space: Future[Information] = Future { getFreeSpace }
+            onSuccess(self_space) { self =>
+              {
+                config.remote.find(x => x.pubkey == req.pub) match { //find the host that has the same pubkey
+                  case Some(remote) =>
+                    if (self - req.space > Bytes(0)) {
+                      complete(remote.hostname)
+                      //TODO check quota of that pubkey
+                      //TODO Allocate space
+                      //TODO send POST to requestor
+                      //TODO rollback
+                    } else {
+                      complete(StatusCodes.InsufficientStorage)
+                    }
+                  case None =>
+                    Logger.trace(
+                      s"Client with pubkey ${req.pub} tried to aquire ${req.space}, denied as pubkey didn't match")
+                    complete(StatusCodes.Forbidden)
+                }
+              }
             }
           }
         }
+      } ~ path("allocate" / "done") {
+        post {
+          entity(as[AllocateResponse]) { req =>
+            if (req.status) {
+              complete("ast")
+              //TODO respond with intent
+              //TODO begin the rsync
+              //TODO send the response of a completed transfer and bytecount
+            } else {
+              //TODO fail
+              complete("lol")
+            }
+          }
+        }
+      } ~ path("sync") {
+        //TODO register sync state start
+        complete("TODO")
+      } ~ path("sync" / "done") {
+        //TODO register sync state complete
+        complete("TODO")
       }
     route
   }
-
+  def getFreeSpace: Information = {
+    Information(config.local.maxSpace).get - Bytes(
+      new File(config.local.recvPath).getTotalSpace)
+  }
   def start() {
     val bind = Http()
-      .bindAndHandle(routes(), "localhost", config.app.port)
+      .bindAndHandle(routes(), "localhost", config.app.port) //TODO Https
     Logger.info(s"Server started on port ${config.app.port}")
     StdIn.readLine() //TODO replace
     bind
--- src/main/scala-2.12/mortar/Spec.scala
+++ src/main/scala-2.12/mortar/Spec.scala
@@ -6,16 +6,13 @@ import akka.http.scaladsl.marshalling.ToEntityMarshaller
 import akka.http.scaladsl.unmarshalling.FromEntityUnmarshaller
 import spray.json._
 
-
-
 //Case classes to define application
 case class ApplicationConfig(app: AppConfig,
-                             remote: Option[List[RemoteMachine]],
-                             local: LocalMachine)//,
+                             remote: List[RemoteMachine],
+                             local: LocalMachine) //,
 //rsync: RSyncConfig)
 
-case class AppConfig(port: Int,
-                     maxSpace: Option[String])
+case class AppConfig(port: Int, maxSpace: Option[String])
 
 case class Security(luksCipher: String,
                     luksHash: String,
@@ -25,7 +22,7 @@ case class Security(luksCipher: String,
 case class SSHConfig(pub: String, priv: String)
 
 case class RemoteMachine(hostname: String,
-                         Interval: String,
+                         interval: String,
                          port: Int,
                          toFile: Option[String],
                          recvFile: Option[String],
@@ -35,23 +32,28 @@ case class LocalMachine(uname: String,
                         recvPath: String,
                         toShoot: List[String],
                         sec: Security,
-                        maxSpace: Option[String])
+                        maxSpace: String,
+                        known_hosts: Option[Boolean]) //TODO
 
 case class RSyncConfig(opts: Option[List[String]])
-
 
 //Web request safe SerDes
 final case class FreeSpace(space: Information) {
   implicit object FreeSpaceFormat extends RootJsonFormat[FreeSpace] {
     override def read(json: JsValue): FreeSpace = json match {
-      case JsObject(obj) => obj.get("space")
-                               .asInstanceOf[Option[Double]] match {
-        case Some(size) => FreeSpace(Bytes(size))
-        case None => deserializationError(s"space field not in FreeSpace map, got ${obj.toMap} instead")
-      }
+      case JsObject(obj) =>
+        obj
+          .get("space")
+          .asInstanceOf[Option[Double]] match {
+          case Some(size) => FreeSpace(Bytes(size))
+          case None =>
+            deserializationError(
+              s"space field not in FreeSpace map, got ${obj.toMap} instead")
+        }
       case x => deserializationError(s"Need an Information in Bytes, got ${x}")
     }
-    override def write(obj: FreeSpace) = JsObject(Map("space" -> JsNumber(obj.space.toBytes)))
+    override def write(obj: FreeSpace) =
+      JsObject(Map("space" -> JsNumber(obj.space.toBytes)))
   }
   def -(that: FreeSpace): FreeSpace = {
     FreeSpace(this.space - that.space)
@@ -59,6 +61,9 @@ final case class FreeSpace(space: Information) {
 }
 
 final case class PubKey(key: String)
+final case class SpaceRequest(pub: String, space: Information)
+final case class AllocateResponse(space: Information, status: Boolean)
+final case class SynchronizeResponse()
 
 trait MortarJsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit object InformationJsonFormat extends RootJsonFormat[Information] {
@@ -71,5 +76,6 @@ trait MortarJsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   }
   implicit val FreeSpaceFmt = jsonFormat(FreeSpace, "space")
   implicit val PubKeyFmt = jsonFormat1(PubKey)
+  implicit val SpaceRequestFmt = jsonFormat2(SpaceRequest)
+  implicit val AllocateResponseFmt = jsonFormat2(AllocateResponse)
 }
-
